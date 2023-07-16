ddeabd7ab07091fa8e92442361930a456e72a740
Suene <suene.vs@gmail.com>
2023-07-16 16:25:29 -0300

    :tada: Commit inicial

---
+++ exercicio-01/script.js
@@ -0,0 +1,4 @@
+// 1. Faça um programa que mostre a contagem de 1 a 10.
+ for (let i=1; i<=10; i++) {
+    console.log(i);
+ }
---
+++ exercicio-04/script.js
@@ -0,0 +1,21 @@
+// 4. Faça um programa que mostre todos os números primos de 0 a 100.
+
+function isPrime(n) {
+    let dividers = 0;
+    for (let i=0; i<=n; i++) {
+        if (n % i === 0) {
+            dividers ++;
+        }
+    }
+    if (dividers === 2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+for(let i=1; i<=100; i++) {
+    if (isPrime(i)) {
+        console.log(i);
+    }
+}
